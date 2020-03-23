252701ad83f3dcd26c5273522523f92d80a0405e
code-witch <code-witch@users.noreply.github.com>
2020-03-22 20:07:45 -0600

    Add public folder and a way to access it

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/routes');
-
+const path = require('path');
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 
